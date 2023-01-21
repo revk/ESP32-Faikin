7b6bd3a3d7fee1d7a8cdfb02a9762ccb1124ee33
RevK <github@e.gg>
2023-01-21 15:47:27 +0000

    Started on old daikin control logic

--- main/Daikin.c
+++ main/Daikin.c
@@ -1021,7 +1021,16 @@ static esp_err_t web_get_control_info(httpd_req_t * req)
 
 static esp_err_t web_set_control_info(httpd_req_t * req)
 {
-   // TODO
+	   if (httpd_req_get_url_query_len(req))
+   {
+      char query[1000],value[10];
+      if (!httpd_req_get_url_query_str(req, query, sizeof(query)))
+      {
+	                  if (!httpd_query_key_value(query, "pow", value, sizeof(value)) && *value)
+				  daikin_set_v(power,*value=='1');
+			  // TODO stemp, mode, f_rate
+      }
+   }
    httpd_resp_set_type(req, "text/plain");
    httpd_resp_sendstr(req, "ret=OK,adv=");
    return ESP_OK;
