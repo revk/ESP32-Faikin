2be08a93b13b343a04698d8c02b88638f8f777d7
RevK <github@k.gg>
2023-04-15 14:09:08 +0100

    Check current set

--- main/Daikin.c
+++ main/Daikin.c
@@ -875,7 +875,7 @@ web_head (httpd_req_t * req, const char *title)
                              "input:checked+.slider:before{-webkit-transform:translateX(30px);-ms-transform:translateX(30px);transform:translateX(30px);}"      //
                              "span.slider:before{border-radius:50%;}"   //
                              "span.slider,span.button{border-radius:34px;padding-top:8px;padding-left:10px;border:1px solid gray;box-shadow:3px 3px 3px #0008;}"        //
-			     "select{height:26px;border-radius:50%;background-color:#cff;border:1px solid gray;}" //
+                             "select{height:26px;border-radius:50%;background-color:#cff;border:1px solid gray;}"       //
                              "</style><body><h1>");
    if (title)
       httpd_resp_sendstr_chunk (req, title);
@@ -1813,7 +1813,7 @@ app_main ()
             daikin.mintarget = NAN;
             daikin.maxtarget = NAN;
          }
-         if (!isnan (min) && !isnan (max) && tsample)
+         if (!isnan (current) && !isnan (min) && !isnan (max) && tsample)
          {                      // Monitoring and automation
             if (daikin.lastheat != hot)
             {                   // If we change mode, start samples again
