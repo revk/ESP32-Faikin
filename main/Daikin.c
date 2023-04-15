22740e58d15896b57137a697ba899ee85aa41441
RevK <github@k.gg>
2023-04-15 13:39:05 +0100

    BLE select CSS tidy

--- main/Daikin.c
+++ main/Daikin.c
@@ -875,6 +875,7 @@ web_head (httpd_req_t * req, const char *title)
                              "input:checked+.slider:before{-webkit-transform:translateX(30px);-ms-transform:translateX(30px);transform:translateX(30px);}"      //
                              "span.slider:before{border-radius:50%;}"   //
                              "span.slider,span.button{border-radius:34px;padding-top:8px;padding-left:10px;border:1px solid gray;box-shadow:3px 3px 3px #0008;}"        //
+			     "select{height:26px;border-radius:50%;background-color:#cff;border:1px solid gray;}" //
                              "</style><body><h1>");
    if (title)
       httpd_resp_sendstr_chunk (req, title);
