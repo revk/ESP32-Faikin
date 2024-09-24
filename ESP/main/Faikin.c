1fa8ae5e6af0ef3bd360e58594d688942ef1bd6c
RevK <github@k.gg>
2024-09-24 15:29:31 +0100

    Fix fixstatus logic

--- ESP/main/Faikin.c
+++ ESP/main/Faikin.c
@@ -3874,14 +3874,14 @@ app_main ()
                      jo_stringf (j, "ts", "%04d-%02d-%02dT%02d:%02d:%02dZ", tm.tm_year + 1900,
                                  tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
                   }
-#define	b(name)		if(daikin.status_known&CONTROL_##name){if(!daikin.total##name)jo_bool(j,#name,0);else if(!fixstatus&&daikin.total##name==daikin.statscount)jo_bool(j,#name,1);else jo_litf(j,#name,"%.2f",(float)daikin.total##name/daikin.statscount);} \
+#define	b(name)		if(daikin.status_known&CONTROL_##name){if(!daikin.total##name)jo_bool(j,#name,0);else if(fixstatus||daikin.total##name==daikin.statscount)jo_bool(j,#name,1);else jo_litf(j,#name,"%.2f",(float)daikin.total##name/daikin.statscount);} \
 		  	daikin.total##name=0;
-#define	t(name)		if(daikin.count##name&&!isnan(daikin.total##name)){if(!fixstatus&&daikin.min##name==daikin.max##name)jo_litf(j,#name,"%.2f",daikin.min##name);	\
+#define	t(name)		if(daikin.count##name&&!isnan(daikin.total##name)){if(fixstatus||daikin.min##name==daikin.max##name)jo_litf(j,#name,"%.2f",daikin.min##name);	\
 		  	else {jo_array(j,#name);jo_litf(j,NULL,"%.2f",daikin.min##name);jo_litf(j,NULL,"%.2f",daikin.total##name/daikin.count##name);jo_litf(j,NULL,"%.2f",daikin.max##name);jo_close(j);}}	\
 		  	daikin.min##name=NAN;daikin.total##name=0;daikin.max##name=NAN;daikin.count##name=0;
-#define	r(name)		if(!isnan(daikin.min##name)&&!isnan(daikin.max##name)){if(!fixstatus&&daikin.min##name==daikin.max##name)jo_litf(j,#name,"%.2f",daikin.min##name);	\
+#define	r(name)		if(!isnan(daikin.min##name)&&!isnan(daikin.max##name)){if(fixstatus||daikin.min##name==daikin.max##name)jo_litf(j,#name,"%.2f",daikin.min##name);	\
 			else {jo_array(j,#name);jo_litf(j,NULL,"%.2f",daikin.min##name);jo_litf(j,NULL,"%.2f",daikin.max##name);jo_close(j);}}
-#define	i(name)		if(daikin.status_known&CONTROL_##name){if(!fixstatus&&daikin.min##name==daikin.max##name)jo_int(j,#name,daikin.total##name/daikin.statscount);     \
+#define	i(name)		if(daikin.status_known&CONTROL_##name){if(fixstatus||daikin.min##name==daikin.max##name)jo_int(j,#name,daikin.total##name/daikin.statscount);     \
                         else {jo_array(j,#name);jo_int(j,NULL,daikin.min##name);jo_int(j,NULL,daikin.total##name/daikin.statscount);jo_int(j,NULL,daikin.max##name);jo_close(j);}       \
                         daikin.min##name=0;daikin.total##name=0;daikin.max##name=0;}
 #define e(name,values)  if((daikin.status_known&CONTROL_##name)&&daikin.name<sizeof(CONTROL_##name##_VALUES)-1)jo_stringf(j,#name,"%c",CONTROL_##name##_VALUES[daikin.name]);
